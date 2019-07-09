b0d736f895462b1fc54d216f59770a17e2b09a6f
STAS Puts <sputseyko@mail.ru>
2019-07-09 18:42:24 +0300

    Add project files.

---
+++ Task_from_ElateSoftware/GenericStack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+
+namespace Task_from_ElateSoftware
+{
+    public class GenericStack
+    {
+        private object[] Array { get; set; }
+        private int higherCell = -1;
+
+        public GenericStack(int inputSize)
+        {
+            Array = new object[inputSize];
+        }
+
+        public void Push(object element)
+        {
+            for (int idCell = 0; idCell < Array.Length; idCell++)
+            {
+                if (Array[idCell] == null)
+                {
+                    Array[idCell] = element;      //put element on the array
+                    higherCell++;                 //remember height of the array
+                    break;
+                }
+            }
+        }
+
+        public object GetOut()
+        {
+            if(higherCell == -1)
+            {
+                Console.WriteLine("Stack is null!!!");
+                return null;
+            }
+            object element = Array[higherCell];
+            Array[higherCell] = null;
+            higherCell--;
+            return element;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("The last element is: " + Array[higherCell]);
+        }
+
+        public override string ToString()
+        {
+            string answer = "";
+            foreach(var cell in Array)
+            {
+                answer = answer + " " + cell;
+            }
+            return answer;
+        }
+    }
+}
---
+++ Task_from_ElateSoftware/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_from_ElateSoftware
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericStack stack = new GenericStack(3);
+            stack.Push(2);
+            stack.Info();
+            stack.Push("Hello!");
+            stack.Info();
+            Console.WriteLine(stack.ToString());
+            Console.WriteLine(stack.GetOut());
+            Console.WriteLine(stack.GetOut());
+            Console.WriteLine(stack.GetOut());
+            Console.WriteLine(stack.GetOut());
+        }
+    }
+}
